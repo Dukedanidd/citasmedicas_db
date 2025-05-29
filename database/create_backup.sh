e7e17fe395d9d67bc39c0231cc7914be05d82be8
bueno-armando <armandoseb.bueno@gmail.com>
2025-05-29 13:34:53 -0600

    Improve FDB setup script

--- database/create_backup.sh
+++ database/create_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # crea un backup *completo* de la BD con mariadb-dump 
+cd /home/senorbuen0/ISC/sem6/bd/proyecto/citasmedicas_db/database
 /opt/lampp/bin/mariadb-dump -u root -p --routines --triggers --events --single-transaction clinica_db > backup_completo_clinica_$(date +%Y%m%d_%H%M%S).sql
